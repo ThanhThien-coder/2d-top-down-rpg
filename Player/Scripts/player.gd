0c6e80f0533b8f228406bdfec116f6b195908647
ThanhThien-coder <nguyenthanhthien2432006@gmail.com>
2025-11-21 00:21:35 +0700

    thêm tính năng di chuyển cho player

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,5 +1,5 @@
+class_name Player
 extends CharacterBody2D
-
 
 const SPEED = 200.0
 var cardinal_direction: Vector2 = Vector2.DOWN
@@ -17,14 +17,30 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * SPEED
 	move_and_slide()
 	
-	UpdateAnimation()
+	if SetState() || SetDirection():
+		UpdateAnimation()
 	
 	
-
+# xác đinh hướng quay mặt hiện tại của nhân vật
 func SetDirection() -> bool:
+	var new_dir: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+		
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true 
 	
 
+# Xác định trạng thái nhân vật đang "Idle" hay đang "Walk" 
 func SetState() -> bool:
 	var newState: String
 	if direction == Vector2.ZERO:
@@ -39,12 +55,13 @@ func SetState() -> bool:
 	return true
 	
 
+# Cập nhật animation cho nhân vật dựa theo trạng thái và hướng quay mặt của nhân vật
 func UpdateAnimation() -> void:
-	animation_player.play( state + "_" + DicrTranslate() )
+	animation_player.play( state + "_" + DirTranslate() )
 	pass
 	
 
-func DicrTranslate() -> String:
+func DirTranslate() -> String:
 	if cardinal_direction == Vector2.DOWN:
 		return "Down"
 	elif cardinal_direction == Vector2.UP:
